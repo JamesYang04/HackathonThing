120b55d193ead645fb21d04d7da2a2eae218de18
alinsanity16 <alanlin@andrew.cmu.edu>
2023-02-03 19:51:46 -0500

    V1

---
+++ main.py
@@ -0,0 +1,9 @@
+from generaterecipe import generate
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+test = generate()
+ingredients = ['flour', 'butter', 'sugar', 'apples', 'chicken', 'salmon', 'salt', 'vinegar']
+Ingredients= ["butter", "sugar", "750g luxury mixed dried fruit (one that includes mixed peel and glac\u00e9 cherries)", "finely grated zest and juice of 1 orange", "finely grated zest of 1 lemon", "100ml/3\u00bd fl oz cherry brandy or brandy plus 4tbsp more", "85g macadamia nut", "3 large eggs, lightly beaten", "85g ground almond", "200g plain flour", "\u00bd tsp baking powder", "1 tsp ground mixed spice", "1 tsp ground cinnamon", "\u00bc tsp ground allspice"]
+recipe = test.generate_recipe(Ingredients)
