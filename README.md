a0aeca40dc429a3faff063dce1687a71b7b5bfb5
JamesYang04 <jyang112004@gmail.com>
2023-02-03 22:23:48 -0500

    Updated generaterecipe
    added assumed ingredients, more intelligent database search

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Program which searches databases for recipes, given a set of grocery materials, 
 Developed as part of CMU TartanHacks 2023 (James Yang, Alan Lin, Max Hu, Sophie Feng, collectively known as team Canadian Geese).
 
 # Dataset
-Download the recipes dataset here: https://drive.google.com/file/d/1RrCHyl7BqPEDS33FRhFzN9RawMB18S7e/view?usp=sharing
+Download the recipes dataset [here](https://drive.google.com/file/d/1RrCHyl7BqPEDS33FRhFzN9RawMB18S7e/view?usp=sharing)
--- generaterecipe.py
+++ generaterecipe.py
@@ -7,27 +7,38 @@ class generate:
         f = open('recipes_with_nutritional_info.json')
         data = json.load(f)
 
+        assumed = ["leavening agents", "spices", "wheat flour", "sugars", "water", "oil", "butter", "salt", "rice flour", "vinegar", "honey"]
         for dish in data:
             need = dish['ingredients']
             can = True
+            customCount = 0
             for t in need:
                 found = False
                 string = t['text']
+                sList = string.split(", ")
                 for ing in ingredients:
-                    if ing in string:
+                    if sList[0] in assumed:
                         found = True
                         break
-                if not found:
+                    if ing in sList[0]:
+                        found = True
+                        customCount += 1
+                        break
+                    if sList[0] in ing:
+                        found = True
+                        customCount += 1
+                        break
+                if not found or customCount == 0:
                     can = False
                     break
             if can:
                 print("We can make " + dish['title'])
-                print("Follow the following recipe: ")
-                i = 1
-                for steps in dish['instructions']:
-                    step = steps['text']
-                    print(str(i) +  ")" + step)
-                    i+=1
+                print("using these ingredients: ")
+                for i in range(1, len(dish['ingredients']) + 1):
+                    s = dish['ingredients'][i-1]['text']
+                    qty = dish['quantity'][i-1]['text']
+                    unit = dish['unit'][i-1]['text']
+                    print(str(i) +  ") " + qty + " " + unit + " " + s)
                 print("\n")
 
 
